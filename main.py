8d7f9294bb9cfda3a5f8f151ba88d0900c99b62b
Natalka <osinska.natalia@outlook.com>
2023-11-20 10:24:39 +0100

    added chose_classes and login function

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-#checking time
-import schedule
-from datetime import time
+#scheduling in shell
 
-begin_time = time(hour= , minute= ,second= )
-schedule.every().friday.at(begin_time).do(connect) # ew trzeba będzie zmienić dzień 
-schedule.every().friday.at(begin_time).do(book)
-
-#connect to website
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
-# By class is used to locate elements within a document
 
+url = 'https://fitness-academy.com.pl/kluby-fitness/wroclaw-maslicka/grafik-zajec'
+#xpath do rezerwacji, login, password
 def connect(url) -> bool:
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome()
     driver.get(url)
 
-    element = driver.find_element(By., )
-    element.clear( ) # czyści jeśli coś jest wcześniej wpisane w okienko
+def chose_classes():
+    reserve = driver.find_element(By.XPATH, xpath) #chyba, że tego xpath da sie automatycznie generowac
+    reserve.click()
 
-    element.send_keys(Keys.ENTER) # wciska klawisz, można to robić na każdym elem dzięki czemu można stosować skróty klawiszowe
-    driver.find_element(By.ID, "submit").click() # klika przycisk
-    driver.close()
+def login():
+    text_login = driver.find_element(By.ID, "member_login_form_email")
+    text_login.clear()
+    text_login.send_keys(login)
+
+    text_password = driver.find_element(By.ID, "member_login_form_password")
+    text_password.clear()
+    text_password.send_keys(password)
+
+    driver.find_element(By.ID, "member_login_form_submit").click()
 
 
-#booking
-def book():
-    pass()
+driver.close()
