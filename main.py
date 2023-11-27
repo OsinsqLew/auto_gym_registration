59d7d6f3b42ce7b5fa47fe85e241a6313d9e2d92
OsinsqLew <106916621+OsinsqLew@users.noreply.github.com>
2023-11-27 14:25:48 +0100

    fixed class, added config file

--- main.py
+++ main.py
@@ -2,28 +2,43 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.actions import Actions
 import argparse
 
 url = 'https://fitness-academy.com.pl/kluby-fitness/wroclaw-maslicka/grafik-zajec'
-#xpath do rezerwacji, login, password
+#xpath do rezerwacji
 
 parser = argparse.ArgumentParser()
 args = parser.parse_args()
 
-class Book_Classes(xpath, url)
+class BookClasses(xpath, url):
     def __init__(self, xpath):
         self.xpath = xpath
 
-    def connect(url) -> None:
+    def reserve(self): -> None:
+    '''
+
+        Flow:
+        1. Connect to website
+        2. Select classes by Xpath
+        3. Log in
+            3.1 Access config file
+            3.2 Input data into textboxes
+
+    '''
         driver = webdriver.Chrome()
         driver.get(url)
 
-    def select_classes() -> None:
-        reserve = driver.find_element(By.XPATH, xpath) #chyba, że tego xpath da sie automatycznie generowac
+        reserve = driver.find_element(By.XPATH, self.xpath)  # chyba, że tego xpath da sie automatycznie generowac
         reserve.click()
 
-    def log_in() -> None:
+        # tu musimy przerzucić się na komunikat/strone do logowania, który występuje
+
+
+
+        # tu pobieramy z pliku config haslo i login
+
+
         text_login = driver.find_element(By.ID, "member_login_form_email")
         text_login.clear()
         text_login.send_keys(login)
@@ -34,12 +49,9 @@ class Book_Classes(xpath, url)
 
         driver.find_element(By.ID, "member_login_form_submit").click()
 
-
-driver.close()
+        driver.close()
 
 if __name__ == __main__:
-    classes = Book_Classes()
-    classes.connect()
-    classes.select_classes()
-    classes.log_in()
+    classes = BookClasses()
+    classes.reserve()
 
