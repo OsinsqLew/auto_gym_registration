06fab337954e2401f3f4796f5c12ecd08d4a1b98
OsinsqLew <106916621+OsinsqLew@users.noreply.github.com>
2023-11-27 11:13:16 +0100

    made a class from previous version

--- main.py
+++ main.py
@@ -3,27 +3,43 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
+import argparse
 
 url = 'https://fitness-academy.com.pl/kluby-fitness/wroclaw-maslicka/grafik-zajec'
 #xpath do rezerwacji, login, password
-def connect(url) -> bool:
-    driver = webdriver.Chrome()
-    driver.get(url)
 
-def chose_classes():
-    reserve = driver.find_element(By.XPATH, xpath) #chyba, że tego xpath da sie automatycznie generowac
-    reserve.click()
+parser = argparse.ArgumentParser()
+args = parser.parse_args()
 
-def login():
-    text_login = driver.find_element(By.ID, "member_login_form_email")
-    text_login.clear()
-    text_login.send_keys(login)
+class Book_Classes(xpath, url)
+    def __init__(self, xpath):
+        self.xpath = xpath
 
-    text_password = driver.find_element(By.ID, "member_login_form_password")
-    text_password.clear()
-    text_password.send_keys(password)
+    def connect(url) -> None:
+        driver = webdriver.Chrome()
+        driver.get(url)
 
-    driver.find_element(By.ID, "member_login_form_submit").click()
+    def select_classes() -> None:
+        reserve = driver.find_element(By.XPATH, xpath) #chyba, że tego xpath da sie automatycznie generowac
+        reserve.click()
+
+    def log_in() -> None:
+        text_login = driver.find_element(By.ID, "member_login_form_email")
+        text_login.clear()
+        text_login.send_keys(login)
+
+        text_password = driver.find_element(By.ID, "member_login_form_password")
+        text_password.clear()
+        text_password.send_keys(password)
+
+        driver.find_element(By.ID, "member_login_form_submit").click()
 
 
 driver.close()
+
+if __name__ == __main__:
+    classes = Book_Classes()
+    classes.connect()
+    classes.select_classes()
+    classes.log_in()
+
